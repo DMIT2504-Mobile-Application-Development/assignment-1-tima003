ef43a1290106baff6c7b43e229fb83a9648d0f41
fatima <fatima.khalif2003@gmail.com>
2025-05-19 13:48:53 -0600

    services

---
+++ lib/weather_api.dart
@@ -0,0 +1,23 @@
+import 'dart:convert'
+
+import 'dart:io'
+import 'package:http/http.dart' as http
+
+const String weatherApiKey = 'YOUR_API_KEY_HERE'
+const String currentWeatherEndpoint = 'https://api.openweathermap.org/data/2.5/weather'
+
+Future<dynamic> getWeatherForCity({required String city}) async {
+  final url = '$currentWeatherEndpoint?units=metric&q=$city&appid=$weatherApiKey'
+
+  try {
+    final response = await http.get(Uri.parse(url))
+
+    if (response.statusCode != 200) {
+      throw Exception('There was a problem with the request: status ${response.statusCode} received')
+    }
+
+    return jsonDecode(response.body)
+  } catch (e) {
+    throw Exception('There was a problem with the request: $e')
+  }
+}
